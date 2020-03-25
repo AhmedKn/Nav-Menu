5b732d8c7724d6a3ac5de2b5ed57d8430845d555
AhmedKn <ahmedkanoun03@gmail.com>
2020-03-25 11:05:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const navcont = [
+  {
+    name:'Home',
+    ref:'https://www.home.com/',
+  },
+  {
+    name:'Service',
+    ref:'https://www.service.com/',
+    drop :[
+      {
+        name:'For entrepreneurs',
+        ref:'https://ent.com/'
+      },
+      {
+        name:'For student',
+        ref:'https://ent.com/'
+      },
+      {
+        name:'For hobbyists',
+        ref:'https://ent.com/'
+      }
+    ]
+  },
+  {
+    name:'Contact',
+    ref:'https://www.contact.com/',
+  },
+  
+]
+
+
+function Fill(props){
+  const navecontent = props.navcontent
+  let newarr=[]
+  for (let i of navecontent)
+     {
+       // eslint-disable-next-line
+     {i.drop ? (newarr.push(
+         <li key={i} className="navkey" ID="drop"><a href={i.ref} className="navkey">{i.name}</a>
+        <ul className="dropdown-menu">{i.drop? i.drop.map((el,j) =><li key={j} className="dropdownkey"><a href={el.ref} className="dropdownkey1">{el.name}</a></li>): null}</ul>
+        </li>))
+    
+      
+       : newarr.push(
+          <li key={i} className="navkey" ID="nodrop"><a href={i.ref} className="navkey">{i.name}</a></li>)
+       }
+      }
+     
+    
+return newarr;
+}
+function Navmenu(){
+  return(
+  <div className="test">
+    <ul className="navbar">
+    <Fill navcontent={navcont} />
+    </ul>
+  </div>
+  )
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navmenu />
   );
 }
 
